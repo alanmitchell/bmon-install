02baa1bf0943e2fe64764c90a4a59ca5bc8676e6
Alan Mitchell <tabb99@gmail.com>
2021-03-23 01:30:15 +0000

    Added bootstrap script.

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Ansible scripts for installing the BMON building monitoring software.
 
 ## Installation Procedure
 
+* Have an SMTP server available for use in Django.
 * Ensure a non-root user is available and initiate the install process while logged in
   as that user.
 * Apt update & upgrade
---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Bootstrap script to install Ansible and Git so that the 
+# main BMON install Ansible playbook can be run.
+
+# First get necessary packages with the package manager
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible git nano
+
+# Move to the home directory and clone the BMON install repo,
+# make a .bmon directory to hold variables and other info related
+# to the install, and start a variables file in the directory.
+cd ~
+rm -rf bmon-install
+git clone https://github.com/alanmitchell/bmon-install.git
+mkdir .bmon || true
+cp bmon-install/variables-example.yaml .bmon/variables.yaml
+
+echo "Edit the 'variables.yaml' file in the .bmon directory,"
+echo "for example: nano .bmon/variables.yaml"
+echo "Then, run the BMON installation script:"
+echo
+echo "    bmon-install/install.sh"
